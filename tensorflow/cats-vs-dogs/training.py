5befbb50c00a3f045f79d3a706bcd70c25639938
Jeff Fogarty <jeff@fogarty.org>
2017-09-27 16:12:32 -0500

    added header comments from original code

--- tensorflow/cats-vs-dogs/training.py
+++ tensorflow/cats-vs-dogs/training.py
@@ -1,3 +1,27 @@
+#By @Kevin Xu
+#kevin28520@gmail.com
+#Youtube: https://www.youtube.com/channel/UCVCSn4qQXTDAtGWpWAe4Plw
+#
+#The aim of this project is to use TensorFlow to process our own data.
+#    - input_data.py:  read in data and generate batches
+#    - model: build the model architecture
+#    - training: train
+
+# I used Ubuntu with Python 3.5, TensorFlow 1.0*, other OS should also be good.
+# With current settings, 10000 traing steps needed 50 minutes on my laptop.
+
+# data: cats vs. dogs from Kaggle
+# Download link: https://www.kaggle.com/c/dogs-vs-cats-redux-kernels-edition/data
+# data size: ~540M
+
+# How to run?
+# 1. run the training.py once
+# 2. call the run_training() in the console to train the model.
+
+# Note: 
+# it is suggested to restart your kenel to train the model multiple times 
+#(in order to clear all the variables in the memory)
+# Otherwise errors may occur: conv1/weights/biases already exist......
 
 import os
 import numpy as np
